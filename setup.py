03353ae0d7290453d79c7283a169e801fa9245c7
Antonio Cuni <anto.cuni@gmail.com>
2013-02-08 17:09:31 +0000

    use setuptools if possible (so we can use setup.py develop)

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import sys
 import os
-from distutils.core import setup, Extension
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
 
 ROOT = os.path.dirname(__file__)
 INCLUDE = os.path.join(ROOT, 'include')
